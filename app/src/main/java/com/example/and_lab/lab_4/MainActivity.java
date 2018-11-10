d1ee070b5fbc35710bd3609f069ef7f0416c1cd9
Steven Henry <Stevennassef97@gmail.com>
2018-11-10 17:04:09 +0100

    1- Added MediaPlayer Controller 2- Added VideoView

--- app/src/main/java/com/example/and_lab/lab_4/MainActivity.java
+++ app/src/main/java/com/example/and_lab/lab_4/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.and_lab.lab_4;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import org.w3c.dom.Text;
 
@@ -14,13 +18,33 @@ public class MainActivity extends AppCompatActivity {
     TextToSpeech textToSpeechObj;
     private EditText write;
     private String textToSpeechString;
+    private MediaController controller;
+    private VideoView videoView;
+    private Uri videoUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textToSpeechString = "Playback has completed";
         initializeTextToSpeech();
+        initializeMediaPlayer();
+//        initializeMediaController();
 
+    }
+
+    protected void initializeMediaPlayer(){
+        videoUri = Uri.parse("game_sound.wav");
+        videoView.setVideoURI(videoUri);
+        videoView.start();
+    }
+
+    protected void initializeMediaController(){
+        MediaController controller = new MediaController(this);
+        this.videoView = findViewById(R.id.videoView);
+        this.controller.setAnchorView(this.videoView);
+        controller.setMediaPlayer(this.videoView);
+        this.videoView.setMediaController(controller);
     }
 
     protected void initializeTextToSpeech(){
