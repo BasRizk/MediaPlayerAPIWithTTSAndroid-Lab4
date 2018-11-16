fac709ebb72b7c73c752d03835b4180930b76a2c
Basem Rizk <basem.rizk@outlook.com>
2018-11-16 23:43:52 +0100

    Video player working with basic media controller

---
+++ app/src/main/java/com/example/and_lab/lab_4/CustomMediaController.java
@@ -0,0 +1,12 @@
+package com.example.and_lab.lab_4;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+public class CustomMediaController extends MediaController {
+
+    public CustomMediaController(Context context) {
+        super(context);
+    }
+}
